8dda8d20489ee9ba219607261f29ed027bb5d722
hwgiwh <hwgiwh271@mobility42.io>
2024-04-03 01:29:44 +0900

    ..

--- src/COCI06 /0602Herman.kt
+++ src/COCI06 /0602Herman.kt
@@ -1,7 +1,8 @@
 
 /** https://dmoj.ca/problem/coci06c1p2 */
 fun main() {
-    val input = readln().toInt()
+    val input = readln().toIntOrNull() ?: throw error("input은 정수여야 합니다.")
+    if(input < 0 || input > 10000) throw error("input은 0보다 크거나 같고 10000보다 작거나 같아야 합니다.")
     /** 유클리드에서의 원 면적*/
     println("%.6f".format((Math.PI * input * input)))
     /** taxicab 이론에서의 원 면적 */
